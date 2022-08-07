19a84d1e2cc49cd52590dda1a80285b14b280719
ERKELSOT <1256029807@qq.com>
2022-08-07 09:23:28 +0800

    ✨ feat(outputfile): test

--- packages/devui-cli/src/index.ts
+++ packages/devui-cli/src/index.ts
@@ -1,10 +1,9 @@
 #!/usr/bin/env node
-// import program from './core/program'
-// import viteCliCoreCommand from './core/command'
-// async function createViteCliCommand() {
-//   await viteCliCoreCommand()
-//   program.parse(process.argv)
-// }
+import program from './core/program'
+import viteCliCoreCommand from './core/command'
+async function createViteCliCommand() {
+  await viteCliCoreCommand()
+  program.parse(process.argv)
+}
 
-// createViteCliCommand()
-console.log(2312313231)
+createViteCliCommand()
